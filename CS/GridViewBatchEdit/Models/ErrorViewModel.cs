95bf1700b40b2b58d46550b482ccef4ac2c3024e
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 14:15:43 +0400

    Change example status to out of maintenance

--- CS/GridViewBatchEdit/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace GridViewBatchEdit.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
